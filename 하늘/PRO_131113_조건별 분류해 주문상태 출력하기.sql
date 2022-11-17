eb1f73334441ac0573d5d3fa034378bf1ee4b8fc
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-11-17 14:53:40 +0900

    Delete PRO_131113_조건별 분류해 주문상태 출력하기.sql

--- 하늘/PRO_131113_조건별 분류해 주문상태 출력하기.sql
+++
@@ -1,6 +0,0 @@
-SELECT order_id, product_id, date_format(out_date,'%Y-%m-%d' ),
-      case when out_date <='2022-05-01' then '출고완료'
-      when out_date >'2022-05-01' then '출고대기'
-      when out_date is null then '출고미정'
-      end '출고여부'
-from food_order
