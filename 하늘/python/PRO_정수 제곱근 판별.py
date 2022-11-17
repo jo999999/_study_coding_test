79180b2342424731fbbdd37a8032e9132cf7a9d1
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-11-17 16:11:20 +0900

    Create PRO_정수 제곱근 판별.py

---
+++ 하늘/python/PRO_정수 제곱근 판별.py
@@ -0,0 +1,20 @@
+import math # math.sqrt()이거 이용하면 됨!
+
+def solution(n):
+    if math.sqrt(n) == round(math.sqrt(n)):
+        answer = (math.sqrt(n)+1)**2
+    else:
+        answer = -1
+    return answer
+
+
+
+#def solution(n):
+#    for i in range( 0, round(n/2)): # round(n/2) 때문에 런타임 에러 나는듯.
+#        if i**2 == n:
+#            answer = (i+1)**2
+#            break
+#        else:
+#            answer = -1
+
+ #   return answer
