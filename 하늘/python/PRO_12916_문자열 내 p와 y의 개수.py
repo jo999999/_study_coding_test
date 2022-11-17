81712b486a03411897659ae7894e5183a7374e87
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-11-17 16:22:21 +0900

    Create PRO_12916_문자열 내 p와 y의 개수.py

---
+++ 하늘/python/PRO_12916_문자열 내 p와 y의 개수.py
@@ -0,0 +1,18 @@
+def solution(s):
+    s = s.lower()
+    p_lst = [i for i in s if i =='p']
+    y_lst = [i for i in s if i =='y']
+    
+    if len(p_lst) == len(y_lst):
+        return True
+    else:
+        return False 
+        
+        
+        
+# 더 간결히 푸는 법  # count()함수 이용!!!!!!!!!!
+# def solution(s):
+#     if s.lower().count('p') == s.lower().count('y'):
+#         return True
+#     else:
+#         return False
