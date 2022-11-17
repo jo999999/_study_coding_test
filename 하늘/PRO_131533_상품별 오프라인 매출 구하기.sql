6b7b3407ed382f4e247efc1c554347695cb2e37a
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-11-17 14:53:46 +0900

    Delete PRO_131533_상품별 오프라인 매출 구하기.sql

--- 하늘/PRO_131533_상품별 오프라인 매출 구하기.sql
+++
@@ -1,8 +0,0 @@
-select p.product_code, (p.price * a.sales_amount ) as sales
-from product p 
-left join
-    (select product_id, sum(sales_amount) as sales_amount 
-    from offline_sale 
-    group by product_id) as a on p.product_id= a.product_id
-group by product_code
-order by sales desc, product_code asc
