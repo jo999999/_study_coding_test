cd602c3c3648ce1710cc5fa391563314a5f7087d
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-11-17 15:04:54 +0900

    Create PRO_12928_약수의 합.py

---
+++ 하늘/python/PRO_12928_약수의 합.py
@@ -0,0 +1,23 @@
+def solution(n):
+    lst = []
+    for i in range(0,n+1):
+        for j in range(0, n+1):
+            if i *j == n :
+                lst.append(i)
+                lst.append(j)
+    lst = list(set(lst))
+    answer = sum(lst)
+    return answer
+    
+# 괜한 이중 포문 말고 더 간단한 풀이
+#
+#def solution(n):
+#  lst = []
+#    for i in range(1, n+1):
+#        if n % i == 0: # 나머지가 0
+#            lst.append(i)
+#   
+#    answer = sum(lst)
+#    return answer
+    
+   
