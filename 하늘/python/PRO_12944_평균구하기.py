4da6e4d193597a0748f88beed12ad4be63b5b1ee
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-11-17 14:51:48 +0900

    Create PRO_12944_평균구하기.py

---
+++ 하늘/python/PRO_12944_평균구하기.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = 0
+    for i in arr:
+        answer +=i
+    answer = answer / len(arr)
+    return answer
+    
+  # 그냥 sum()하면 될것을 ㅎ
+  # def average(list):
+  #  return (sum(list) / len(list))
