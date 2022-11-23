9f8c0d814bbb162eeb8dccd2596b6ecbc2956497
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-11-23 15:26:19 +0900

    Create PRO[Lv1]_.py

---
+++ 하늘/python/PRO[Lv1]_.py
@@ -0,0 +1,21 @@
+def solution(s):
+    try:
+        int(s)
+    except:
+        return False
+      
+    return len(s) == 4 or len(s) == 6  # 문제 조건 잘읽자. ::: 문자열 s의 길이가 4 혹은 6이고
+
+
+
+# 아! try - except 이거 생각을 못했네
+# def alpha_string46(s):
+#     try:
+#         int(s)
+#     except:
+#         return False
+        
+        
+ # isdigit() 도 있음 ! 
+ # def alpha_string46(s):
+ #    return s.isdigit() and len(s) in (4, 6)
