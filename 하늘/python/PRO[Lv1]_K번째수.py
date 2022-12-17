8b872ae7900b70751ae7749dc38e6d4edee8cd5a
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-12-18 00:39:33 +0900

    Create PRO[Lv1]_K번째수.py

---
+++ 하늘/python/PRO[Lv1]_K번째수.py
@@ -0,0 +1,6 @@
+def solution(array, commands):
+    answer = []
+    for part in commands:
+        sort = sorted(array[part[0]-1:part[1]])
+        answer.append(sort[part[2]-1])    
+    return answer
