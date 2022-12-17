6400dc94bc707be4f67a2cdc15441be1ad6f34b8
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-12-17 23:40:58 +0900

    Create PRO[Lv1]_예산(한번더풀기!).py

---
+++ 하늘/python/PRO[Lv1]_예산(한번더풀기!).py
@@ -0,0 +1,30 @@
+def solution(d, budget):
+    if sum(d) <= budget:
+        answer = len(d)
+    else:        
+        d.sort()
+        cnt =0
+        for i in range(len(d)): 
+            budget -= d[i] ## 여기 위아래 순서땜시 !
+            if budget >=0: ##
+                cnt+=1 
+            else:
+                break
+    
+        answer = cnt
+    return answer
+  
+'''
+# 또다른 풀이
+def solution(d, budget):
+    d.sort()
+    cnt = 0
+
+    for i in d :
+        budget -= i
+        if budget < 0 :
+               break
+        cnt += 1
+    answer = cnt
+    return answer
+'''
