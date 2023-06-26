b568a16e12e19914628f31c52963be3a6dfc4262
haneul jo <77534419+jo999999@users.noreply.github.com>
2023-06-26 19:37:11 +0900

    Create 12924_숫자의 표현.py

---
+++ 하늘/python/LV2/12924_숫자의 표현.py
@@ -0,0 +1,15 @@
+# 연속한 자연수로 표현
+def solution(n):
+    answer = 1
+    ## 앞에서부터 순차적으로 돌아볼까
+    for start in range(1, (n//2)+1): 
+        for i in range(start,n):
+            sum+=i
+            if sum == n:
+                answer +=1
+                break
+                
+            if sum > n: ## 합이 n보다 커지면 반복문 안돌아도 되니까 (효율성 test)
+                break
+
+    return answer
